8e0308b300027a4cd4110aa70b40057f872a29ad
1wqw23r <135705942+1wqw23r@users.noreply.github.com>
2024-09-27 20:46:51 +0300

    Add files via upload

---
+++ PRZ-3.py
@@ -0,0 +1,21 @@
+# PRZ-3
+#1.1
+a= int (input("Введите первое число:"))
+print (a)
+b= int (input("Введите второе число:"))
+print (b)
+c= int (input("Введите третье число:"))
+print (c)
+d= [1,2,3]
+if a in d:
+    print("a принадлежит d")
+else:
+    print("А не принадлежит d")
+if b in d:
+    print("b принадлежит d")
+else:
+    print("b не принадлежит d")
+if c in d:
+    print("c принадлежит d")
+else:
+    print("c не принадлежит d")
